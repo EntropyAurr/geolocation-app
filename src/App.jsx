6f2d97b7a60ccfc497ed489466aeef45ebae9bdd
Au Cao Thuy Duong <auduong.301@gmail.com>
2025-03-17 16:24:05 +0700

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,16 @@
-export default function App() {}
+import { useState } from "react";
+
+export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [countClicks, setCountClicks] = useState(0);
+  const [position, setPosition] = useState({});
+
+  const { lat, lng } = position;
+
+  return (
+    <div>
+      <button>Get my location</button>
+    </div>
+  );
+}
